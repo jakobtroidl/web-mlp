e99a27d2a9ff4d6ab6ca1679018ee5029dc5d0d3
jakobtroidl <jtroidl@g.harvard.edu>
2023-09-18 15:34:46 -0400

    initial setup to work on WebGPU compute shaders

---
+++ main-wonnx.js
@@ -0,0 +1,48 @@
+
+
+import init, { Session, Input } from "@webonnx/wonnx-wasm";
+
+async function fetchBytes(url) {
+  const reply = await fetch(url);
+  const blob = await reply.arrayBuffer();
+  const arr = new Uint8Array(blob);
+  return arr;
+}
+
+async function run() {
+  try {
+    let x = 32768;
+    let y = 102;
+    let model_path="https://jakobtroidl.github.io/data/mlp_128_one_hidden_simplified.onnx";
+
+    const data = Float32Array.from({ length: x * y }, () => Math.random());
+    const [modelBytes] = await Promise.all([
+      fetchBytes(model_path),
+    ]);
+
+    await init();
+    const session = await Session.fromBytes(modelBytes);
+
+    //Start inference
+    const input = new Input();
+    input.insert("x", data);
+
+    for (let i = 0; i < 100; i++) {
+      
+      const start = performance.now();
+      let result = await session.run(input);
+      const end = performance.now();
+
+      document.getElementById("perf").innerHTML = "MLP inference time: " + (end - start) + " ms";
+
+    }
+
+    session.free();
+    input.free();
+
+  } catch (e) {
+    console.error(e, e.toString());
+  }
+}
+
+run();
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,24 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
+
+  plugins: [
+    {
+      name: 'wgsl-loader',
+      resolveId(id) {
+        return id.endsWith('.wgsl') ? id : null;
+      },
+      async load(id) {
+        if (id.endsWith('.wgsl')) {
+          const fs = require('fs').promises;
+          const contents = await fs.readFile(id, 'utf-8');
+          return `export default ${JSON.stringify(contents)}`;
+        }
+        return null;
+      },
+    },
+  ],
+
   assetsInclude: ["**/*.onnx"],
 
   // This is here because of https://github.com/vitejs/vite/issues/7287
