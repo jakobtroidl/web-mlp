069c3c77fcbfd71321a0d57b69d8cec5ac3d9e3b
Jakob Troidl <jakob.troidl@googlemail.com>
2023-09-29 15:05:18 -0400

    fine tune model loader

--- src/modelLoader.js
+++ src/modelLoader.js
@@ -4,37 +4,33 @@ import { linearizeRowMajor } from "./utils";
 // must be a tfjs based model, only including linear/dense layers and activations
 export async function from_tfjs(path) {
   const model = await tf.loadLayersModel(path);
-
-  let weights = [];
-  let biases = [];
-  let activations = [];
-  let weight_shapes = [];
-  let bias_shapes = [];
+  let layers = [];
 
   try {
     model.layers.forEach((layer) => {
       if (layer.getClassName() != "Dense") {
         throw new Error("Only dense layers are supported in WebMLP");
       }
-
       const w = layer.getWeights();
 
       // collect weights
       let w_tensor = w[0];
       let w_array = new Float32Array(linearizeRowMajor(w_tensor.dataSync()));
-      weights.push(w_array);
-      weight_shapes.push(w_tensor.shape);
-
       // collect biases
       let b_tensor = w[1];
       let b_array = new Float32Array(linearizeRowMajor(b_tensor.dataSync()));
-      biases.push(b_array);
-      bias_shapes.push(b_tensor.shape);
 
-      // collect activations
-      activations.push(layer.activation);
+      const layerObject = {
+        weights: w_array,
+        weight_shape: w_tensor.shape,
+        biases: b_array,
+        bias_shape: b_tensor.shape,
+        activation: layer.activation,
+      };
+
+      layers.push(layerObject);
     });
-    return [weights, weight_shapes, biases, bias_shapes, activations];
+    return layers;
   } catch (e) {
     console.error(e);
   }
