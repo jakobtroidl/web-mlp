95b12c934281e177a27657e98809999780f358e6
Simon Warchol <simonwarchol@g.harvard.edu>
2023-10-02 13:48:52 -0400

    Changing build to module

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,24 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  build: {
+    lib: {
+      // Could also be a dictionary or array of multiple entry points
+      entry: "index.js",
+      name: "Web-MLP",
+      // the proper extensions will be added
+      fileName: "web-mlp",
+    },
+    rollupOptions: {
+      input: ["index.js"],
+    },
+    outDir: "dist",
+  },
   // This is here because of https://github.com/vitejs/vite/issues/7287
   optimizeDeps: {
     exclude: ["@webonnx/wonnx-wasm"],
-    esbuildOptions: {
-      target: "es2020",
-    },
+    // esbuildOptions: {
+    //   target: "es2020",
+    // },
   },
 });
