ef4216ed70b50207e883cf0017bbe0bc4e340da7
Jakob Troidl <jakob.troidl@googlemail.com>
2023-10-02 22:23:20 -0400

    use hardware specific limits

--- src/setup.js
+++ src/setup.js
@@ -16,7 +16,32 @@ export async function initWebGPU(ts) {
 
   console.log("tile_size", ts);
 
-  const device = await adapter.requestDevice();
+  const requiredFeatures = [];
+
+  const requiredLimits = {};
+  requiredLimits.maxBufferSize = adapter.limits.maxBufferSize;
+  requiredLimits.maxComputeInvocationsPerWorkgroup =
+    adapter.limits.maxComputeInvocationsPerWorkgroup;
+  requiredLimits.maxComputeWorkgroupSizeX =
+    adapter.limits.maxComputeWorkgroupSizeX;
+  requiredLimits.maxComputeWorkgroupSizeY =
+    adapter.limits.maxComputeWorkgroupSizeY;
+  requiredLimits.maxComputeWorkgroupSizeZ =
+    adapter.limits.maxComputeWorkgroupSizeZ;
+  requiredLimits.maxComputeWorkgroupStorageSize =
+    adapter.limits.maxComputeWorkgroupStorageSize;
+  requiredLimits.maxStorageBufferBindingSize =
+    adapter.limits.maxStorageBufferBindingSize;
+  requiredLimits.maxComputeWorkgroupsPerDimension =
+    adapter.limits.maxComputeWorkgroupsPerDimension;
+
+  const device = await adapter.requestDevice({
+    defaultQueue: {
+      label: "default",
+    },
+    requiredFeatures,
+    requiredLimits,
+  });
 
   const wgslCode = setTileSize(shaderString, ts); // Replace this with your actual WGSL code
   const shaderModule = device.createShaderModule({ code: wgslCode });
