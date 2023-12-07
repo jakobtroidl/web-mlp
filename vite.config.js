61760cb60304c8886f6c01a35d2e1bb3a4f669bb
Jakob Troidl <jakob.troidl@googlemail.com>
2023-12-07 15:22:21 -0500

    bump version

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,11 @@ import { defineConfig } from "vite";
 import { resolve } from "path";
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      util: "util",
+    },
+  },
   build: {
     emptyOutDir: false,
     sourcemap: "inline",
