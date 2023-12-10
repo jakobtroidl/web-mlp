f15868c49df7bdd18cae36ee7bd4e6640df493c5
Jakob Troidl <jakob.troidl@googlemail.com>
2023-12-10 13:50:38 -0500

    feat: add json model loader

--- src/modelLoader.js
+++ src/modelLoader.js
@@ -1,12 +1,45 @@
 import * as tf from "@tensorflow/tfjs";
 import { linearizeRowMajor } from "./utils";
 
+export async function from_json(path) {
+  /**
+   * @param {string} path is a url to a JSON file. See documentation on how the JSON file needs to be structured
+   * @returns {Array} of layer objects describing the MLP
+   */
+  const response = await fetch(path);
+  const mlp_data = await response.json();
+
+  let out_layers = [];
+  try {
+    mlp_data.layers.forEach((layer) => {
+      const layerObject = {
+        weights: layer.weight,
+        weight_shape: layer.weight_shape,
+        biases: layer.bias,
+        bias_shape: layer.bias_shape,
+        activation: mlp_data.activations,
+      };
+
+      out_layers.push(layerObject);
+    });
+    return out_layers;
+  } catch (e) {
+    console.error(e);
+  }
+  return null;
+}
+
 // must be a tfjs based model, only including linear/dense layers and activations
 export async function from_tfjs(path) {
+  // download JSON file from path
+  const response = await fetch(path);
+  const mlp_json = await response.json();
+  console.log("model_json", mlp_json);
+
   const model = await tf.loadLayersModel(path);
   let layers = [];
 
-  console.log(model);
+  console.log("Layers: ", model.layers);
 
   try {
     model.layers.forEach((layer) => {
