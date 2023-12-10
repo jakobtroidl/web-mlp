cbadf02743b4ac99d6f48174d19f0794c8e04d25
Jakob Troidl <jakob.troidl@googlemail.com>
2023-12-10 15:38:50 -0500

    remove tfjs dependency

--- src/modelLoader.js
+++ src/modelLoader.js
@@ -1,6 +1,3 @@
-import * as tf from "@tensorflow/tfjs";
-import { linearizeRowMajor } from "./utils";
-
 export async function from_json(path) {
   /**
    * @param {string} path is a url to a JSON file. See documentation on how the JSON file needs to be structured
@@ -36,49 +33,49 @@ export async function from_json(path) {
   return null;
 }
 
-// must be a tfjs based model, only including linear/dense layers and activations
-export async function from_tfjs(path) {
-  // download JSON file from path
-  const response = await fetch(path);
-  const mlp_json = await response.json();
-  console.log("model_json", mlp_json);
+// // must be a tfjs based model, only including linear/dense layers and activations
+// export async function from_tfjs(path) {
+//   // download JSON file from path
+//   const response = await fetch(path);
+//   const mlp_json = await response.json();
+//   console.log("model_json", mlp_json);
 
-  const model = await tf.loadLayersModel(path);
-  let layers = [];
+//   const model = await tf.loadLayersModel(path);
+//   let layers = [];
 
-  console.log("Layers: ", model.layers);
+//   console.log("Layers: ", model.layers);
 
-  try {
-    model.layers.forEach((layer) => {
-      if (layer.getClassName() != "Dense") {
-        throw new Error("Only dense layers are supported in WebMLP");
-      }
-      const w = layer.getWeights();
+//   try {
+//     model.layers.forEach((layer) => {
+//       if (layer.getClassName() != "Dense") {
+//         throw new Error("Only dense layers are supported in WebMLP");
+//       }
+//       const w = layer.getWeights();
 
-      // collect weights
-      let w_tensor = w[0];
-      let w_array = new Float32Array(linearizeRowMajor(w_tensor.dataSync()));
-      // collect biases
-      let b_tensor = w[1];
-      if (b_tensor == null) {
-        b_tensor = tf.zeros([w_tensor.shape[1]]);
-      }
-      let b_array = new Float32Array(linearizeRowMajor(b_tensor.dataSync()));
+//       // collect weights
+//       let w_tensor = w[0];
+//       let w_array = new Float32Array(linearizeRowMajor(w_tensor.dataSync()));
+//       // collect biases
+//       let b_tensor = w[1];
+//       if (b_tensor == null) {
+//         b_tensor = tf.zeros([w_tensor.shape[1]]);
+//       }
+//       let b_array = new Float32Array(linearizeRowMajor(b_tensor.dataSync()));
 
-      const layerObject = {
-        weights: w_array,
-        weight_shape: w_tensor.shape,
-        biases: b_array,
-        bias_shape: b_tensor.shape,
-        activation: layer.activation.constructor.name,
-      };
+//       const layerObject = {
+//         weights: w_array,
+//         weight_shape: w_tensor.shape,
+//         biases: b_array,
+//         bias_shape: b_tensor.shape,
+//         activation: layer.activation.constructor.name,
+//       };
 
-      layers.push(layerObject);
-    });
-    return layers;
-  } catch (e) {
-    console.error(e);
-  }
+//       layers.push(layerObject);
+//     });
+//     return layers;
+//   } catch (e) {
+//     console.error(e);
+//   }
 
-  return null;
-}
+//   return null;
+// }
